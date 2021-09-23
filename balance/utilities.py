747f668f8002ee21bd8babfdc408255c31f269dd
Felipe Jurado <hola@felipejurado.com>
2021-09-23 20:27:34 +0200

    Añadidos archivos estaticos

---
+++ balance/utilities.py
@@ -0,0 +1,10 @@
+
+
+def fecha_actual(date):
+    months = ("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+    day = date.day
+    month = months[date.month - 1]
+    year = date.year
+    fecha = "{} de {} de {}".format(day, month, year)
+
+    return fecha
--- balance/views.py
+++ balance/views.py
@@ -1,8 +1,12 @@
 from balance import app
+from flask import render_template
+from balance.utilities import fecha_actual
+from datetime import date, datetime
 
 @app.route('/')
 def inicio():
-    return 'Página de inicio'
+    now = datetime.now()
+    return render_template('inicio.html', fecha_hoy = fecha_actual(now))
 
 @app.route('/nuevo', methods=['GET', 'POST'])
 def nuevo():
